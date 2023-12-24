32b1e75e4be0a368ea32513b1ebad668f26eb877
oxahex <oxahex@gmail.com>
2023-12-24 18:26:12 +0900

    init project

---
+++ src/test/java/team/rescue/fridgerescueserver/FridgeRescueServerApplicationTests.java
@@ -0,0 +1,13 @@
+package team.rescue.fridgerescueserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeRescueServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
