3c7089962a8d3acbba7e372a057083193652d6c0
Minhye <amydreamsu@gmail.com>
2023-12-30 23:17:21 +0900

    entity recipe_step

--- src/main/java/team/rescue/fridge/recipe/entity/RecipeStepDto.java
+++ src/main/java/team/rescue/fridge/recipe/entity/RecipeStepDto.java
@@ -4,44 +4,31 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import team.rescue.fridge.member.entity.Member;
 
-
-import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "recipe")
+@Table(name = "recipe_step")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-public class RecipeDto {
+public class RecipeStepDto {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "recipe_id")
+	@Column(name = "recipe_step_id")
 	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "member_id")
-	private Member member;
+	@JoinColumn(name = "recipe_id")
+	private RecipeDto recipe;
 
-	@Column(name = "title", nullable = false, length = 100)
-	private String title;
+	@Column(name = "step_no")
+	private int stepNo;
 
-	@Column(name = "summary", nullable = false, length = 100)
-	private String summary;
+	@Column(name = "step_contents", nullable = false, length = 100)
+	private String stepContents;
 
-	@Column(name = "review_count")
-	private int reviewCount;
-
-	@CreatedDate
-	@Column(name = "created_at", nullable = false)
-	private LocalDateTime createdAt;
-
-	@LastModifiedDate
-	@Column(name = "modified_at")
-	private LocalDateTime modifiedAt;
+	@Column(name = "step_tip", nullable = false, length = 100)
+	private String stepTip;
 }
