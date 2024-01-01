3be0a97e0eabd9b8f595907a8a0db84fca8681a7
oxahex <oxahex@gmail.com>
2024-01-01 22:06:36 +0900

    fix: 생성자 주입되도록 처리

--- src/main/java/team/rescue/fridge/config/RedisConfig.java
+++ src/main/java/team/rescue/fridge/config/RedisConfig.java
@@ -1,6 +1,8 @@
 package team.rescue.fridge.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,29 +11,31 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableCaching
+@RequiredArgsConstructor
 public class RedisConfig {
 
-  @Value("${spring.data.redis.port}")
-  private int port;
+	@Value("${spring.data.redis.port}")
+	private int port;
 
-  @Value("${spring.data.redis.host}")
-  private String host;
+	@Value("${spring.data.redis.host}")
+	private String host;
 
-  @Bean
-  public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory(host, port);
-  }
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory(host, port);
+	}
 
-  @Bean
-  public RedisTemplate<String, Object> redisTemplate() {
-    // redisTemplate를 받아와서 set, get, delete를 사용
-    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-    // setKeySerializer, setValueSerializer 설정
-    // redis-cli을 통해 직접 데이터를 조회 시 알아볼 수 없는 형태로 출력되는 것을 방지
-    redisTemplate.setKeySerializer(new StringRedisSerializer());
-    redisTemplate.setValueSerializer(new StringRedisSerializer());
-    redisTemplate.setConnectionFactory(redisConnectionFactory());
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		// redisTemplate를 받아와서 set, get, delete를 사용
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		// setKeySerializer, setValueSerializer 설정
+		// redis-cli을 통해 직접 데이터를 조회 시 알아볼 수 없는 형태로 출력되는 것을 방지
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
 
-    return redisTemplate;
-  }
+		return redisTemplate;
+	}
 }
