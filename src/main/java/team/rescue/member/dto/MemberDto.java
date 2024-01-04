0b8b706db71d873c3839b9af84b0299b4a842a71
oxahex <oxahex@gmail.com>
2024-01-04 16:46:39 +0900

    feat: 유저 info 응답에 권한 정보 추가

--- src/main/java/team/rescue/member/dto/MemberDto.java
+++ src/main/java/team/rescue/member/dto/MemberDto.java
@@ -2,6 +2,7 @@ package team.rescue.member.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import team.rescue.auth.type.RoleType;
 import team.rescue.member.entity.Member;
 
 public class MemberDto {
@@ -12,11 +13,13 @@ public class MemberDto {
 
 		private Long id;
 		private String nickname;
+		private RoleType role;
 
 		public static MemberInfoDto fromEntity(Member member) {
 			MemberInfoDto memberInfo = new MemberInfoDto();
 			memberInfo.setId(member.getId());
 			memberInfo.setNickname(member.getNickname());
+			memberInfo.setRole(member.getRole());
 
 			return memberInfo;
 		}
