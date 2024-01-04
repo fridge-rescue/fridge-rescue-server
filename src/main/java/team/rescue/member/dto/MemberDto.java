ea6770ad2610e79e682cce9be6e8c163f545cfd1
oxahex <oxahex@gmail.com>
2024-01-04 09:46:59 +0900

    feat: 멤버 DTO 작성

---
+++ src/main/java/team/rescue/member/dto/MemberDto.java
@@ -0,0 +1,24 @@
+package team.rescue.member.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import team.rescue.member.entity.Member;
+
+public class MemberDto {
+
+	@Getter
+	@Setter
+	public static class MemberInfoDto {
+
+		private Long id;
+		private String nickname;
+	}
+
+	public static MemberInfoDto fromEntity(Member member) {
+		MemberInfoDto memberInfo = new MemberInfoDto();
+		memberInfo.setId(member.getId());
+		memberInfo.setNickname(member.getNickname());
+
+		return memberInfo;
+	}
+}
