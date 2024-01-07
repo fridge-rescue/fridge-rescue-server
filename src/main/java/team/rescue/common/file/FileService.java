dd04c39152fceebe1a97c1a374aaee5dcdc2d6ba
oxahex <oxahex@gmail.com>
2024-01-07 16:04:01 +0900

    refact: 다운사이징 된 이미지 URL 반환하도록 교체

--- src/main/java/team/rescue/common/file/FileService.java
+++ src/main/java/team/rescue/common/file/FileService.java
@@ -22,8 +22,15 @@ public class FileService {
 
 	@Value("${cloud.aws.s3.bucket}")
 	private String bucketName;
-	private static final String S3_BUCKET_DIR_PREFIX = "origin/";
+	private static final String S3_BUCKET_UPLOAD_DIR_PREFIX = "origin/";
+	private static final String S3_BUCKET_DOWNLOAD_DIR_PREFIX = "resized/";
 
+	/**
+	 * S3 버킷 이미지 등록
+	 *
+	 * @param image Multipart 이미지 파일
+	 * @return 리사이징 처리 된 이미지 파일 접근 주소
+	 */
 	public String uploadImageToS3(MultipartFile image) {
 
 		log.info("[S3 이미지 업로드] imageName={}", image.getOriginalFilename());
@@ -34,22 +41,26 @@ public class FileService {
 		if (image.getOriginalFilename() == null) {
 			throw new ServiceException(ServiceError.FILE_EXTENSION_INVALID);
 		}
-		String fileName =
-				S3_BUCKET_DIR_PREFIX + ImageUtil.generateImageName(image.getOriginalFilename());
+		String fileName = ImageUtil.generateImageName(image.getOriginalFilename());
 
 		ObjectMetadata objectMetadata = new ObjectMetadata();
 		objectMetadata.setContentType(image.getContentType());
 
 		try {
+
 			PutObjectRequest putObjectRequest = new PutObjectRequest(
-					bucketName, fileName, image.getInputStream(), objectMetadata
+					bucketName,
+					S3_BUCKET_DOWNLOAD_DIR_PREFIX + fileName,
+					image.getInputStream(),
+					objectMetadata
 			);
 			s3Client.putObject(putObjectRequest);
+			
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 
-		return s3Client.getUrl(bucketName, fileName).toString();
+		return s3Client.getUrl(bucketName, S3_BUCKET_DOWNLOAD_DIR_PREFIX + fileName).toString();
 	}
 
 
