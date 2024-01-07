f68e1c0bf6fe706ab0fe2dd23954dc2f6b278983
oxahex <oxahex@gmail.com>
2024-01-07 20:09:02 +0900

    fix: 에러 Enum 정의에서 code 필드 제거

--- src/main/java/team/rescue/error/exception/ServiceException.java
+++ src/main/java/team/rescue/error/exception/ServiceException.java
@@ -7,12 +7,10 @@ import team.rescue.error.type.ServiceError;
 @Getter
 public class ServiceException extends RuntimeException {
 
-	int code;
 	HttpStatus statusCode;
 	String errorMessage;
 
 	public ServiceException(ServiceError serviceError) {
-		this.code = serviceError.getCode();
 		this.statusCode = serviceError.getHttpStatus();
 		this.errorMessage = serviceError.getErrorMessage();
 	}
--- src/main/java/team/rescue/error/type/ServiceError.java
+++ src/main/java/team/rescue/error/type/ServiceError.java
@@ -13,25 +13,24 @@ import org.springframework.http.HttpStatus;
 public enum ServiceError {
 
 	// File
-	FILE_NOT_EXIST(-1, HttpStatus.BAD_REQUEST, "파일을 등록해주세요."),
-	FILE_EXTENSION_INVALID(-1, HttpStatus.BAD_REQUEST, "유효한 파일이 아닙니다."),
-	FILE_RESIZING_FAILURE(-1, HttpStatus.INTERNAL_SERVER_ERROR, "파일 리사이징에 실패했습니다."),
-	FILE_UPLOAD_FAILURE(-1, HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다."),
+	FILE_NOT_EXIST(HttpStatus.BAD_REQUEST, "파일을 등록해주세요."),
+	FILE_EXTENSION_INVALID(HttpStatus.BAD_REQUEST, "유효한 파일이 아닙니다."),
+	FILE_RESIZING_FAILURE(HttpStatus.INTERNAL_SERVER_ERROR, "파일 리사이징에 실패했습니다."),
+	FILE_UPLOAD_FAILURE(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다."),
 
 	// User
-	EMAIL_NOT_FOUND(-1, HttpStatus.NOT_FOUND, "등록되지 않은 이메일입니다."),
-	USER_NOT_FOUND(-1, HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
-	EMAIL_ALREADY_EXIST(-1, HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
-	EMAIL_CODE_MIS_MATCH(-1, HttpStatus.BAD_REQUEST, "이메일 인증 코드가 일치하지 않습니다."),
+	EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "등록되지 않은 이메일입니다."),
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
+	EMAIL_ALREADY_EXIST(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
+	EMAIL_CODE_MIS_MATCH(HttpStatus.BAD_REQUEST, "이메일 인증 코드가 일치하지 않습니다."),
 
 	// Recipe
-	RECIPE_NOT_FOUND(-1, HttpStatus.NOT_FOUND, "이미 삭제된 레시피입니다."),
+	RECIPE_NOT_FOUND(HttpStatus.NOT_FOUND, "이미 삭제된 레시피입니다."),
 
 	// Fridge
-	FRIDGE_NOT_FOUND(-1, HttpStatus.NOT_FOUND, "냉장고 정보가 없습니다.");
+	FRIDGE_NOT_FOUND(HttpStatus.NOT_FOUND, "냉장고 정보가 없습니다.");
 
-
-	private final int code;
+	
 	private final HttpStatus httpStatus;
 	private final String errorMessage;
 }
