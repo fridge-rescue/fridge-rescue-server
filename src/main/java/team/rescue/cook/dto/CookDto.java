ff665fd1d54f0029406e05a94a2e47e890884b45
iseungsoo07 <iseungsoo07@gmail.com>
2024-01-11 13:47:06 +0900

    fix: CookInfoDto의 recipeInfoDto를 builder로 생성하지 않고 of 메서드 사용

--- src/main/java/team/rescue/cook/dto/CookDto.java
+++ src/main/java/team/rescue/cook/dto/CookDto.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import team.rescue.cook.entity.Cook;
-import team.rescue.member.dto.MemberDto.MemberInfoDto;
 import team.rescue.recipe.dto.RecipeDto.RecipeInfoDto;
 
 public class CookDto {
@@ -23,15 +22,7 @@ public class CookDto {
 			return CookInfoDto.builder()
 					.id(cook.getId())
 					.createdAt(cook.getCreatedAt())
-					.recipeInfoDto(RecipeInfoDto.builder()
-							.id(cook.getRecipe().getId())
-							.title(cook.getRecipe().getTitle())
-							.author(MemberInfoDto.builder()
-									.id(cook.getRecipe().getMember().getId())
-									.nickname(cook.getRecipe().getMember().getNickname())
-									.role(cook.getRecipe().getMember().getRole())
-									.build())
-							.build())
+					.recipeInfoDto(RecipeInfoDto.of(cook.getRecipe()))
 					.build();
 		}
 	}
