c8854e8f8317b46169d0115f19664f5e4ff40587
oxahex <oxahex@gmail.com>
2024-01-19 20:42:58 +0900

    test: 검색 기능 테스트

--- src/main/java/team/rescue/search/repository/RecipeSearchRepository.java
+++ src/main/java/team/rescue/search/repository/RecipeSearchRepository.java
@@ -1,11 +1,12 @@
 package team.rescue.search.repository;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.SearchHitSupport;
+import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.data.elasticsearch.core.SearchPage;
 import org.springframework.data.elasticsearch.core.query.Criteria;
 import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
 import org.springframework.data.elasticsearch.core.query.Query;
@@ -32,33 +33,28 @@ public class RecipeSearchRepository {
 	 * Recipe Document 재료 기반 검색
 	 *
 	 * @param ingredients 검색할 재료 문자열
-	 * @param pageRequest 페이지네이션 정보
+	 * @param pageable    페이지네이션 정보
 	 * @return 재료와 매치되는 Recipe Documents
 	 */
-	public SearchPage<RecipeDoc> searchByIngredients(String ingredients, PageRequest pageRequest) {
+	public List<RecipeDoc> searchByIngredients(String ingredients, Pageable pageable) {
 		Criteria criteria = Criteria.where("ingredients").contains(ingredients);
-		Query query = new CriteriaQuery(criteria).setPageable(pageRequest);
+		Query query = new CriteriaQuery(criteria).setPageable(pageable);
 		SearchHits<RecipeDoc> searchHits = searchOperations.search(query, RecipeDoc.class);
-		return SearchHitSupport.searchPageFor(searchHits, query.getPageable());
-//		return searchHit.stream()
-//				.map(SearchHit::getContent)
-//				.collect(Collectors.toList());
 
+		return searchHits.stream().map(SearchHit::getContent).collect(Collectors.toList());
 	}
 
 	/**
 	 * Recipe Document 키워드 기반 검색
 	 *
-	 * @param keyword     검색할 keyword
-	 * @param pageRequest 페이지네이션 정보
+	 * @param keyword  검색할 keyword
+	 * @param pageable 페이지네이션 정보
 	 * @return 키워드와 매치되는 Recipe Documents
 	 */
-	public SearchPage<RecipeDoc> searchByKeyword(String keyword, PageRequest pageRequest) {
-		Criteria criteria = Criteria.where("").contains(keyword);
-		Query query = new CriteriaQuery(criteria).setPageable(pageRequest);
+	public List<RecipeDoc> searchByKeyword(String keyword, Pageable pageable) {
+		Criteria criteria = Criteria.where("ingredients").contains(keyword);
+		Query query = new CriteriaQuery(criteria).setPageable(pageable);
 		SearchHits<RecipeDoc> searchHits = searchOperations.search(query, RecipeDoc.class);
-		return SearchHitSupport.searchPageFor(searchHits, query.getPageable());
+		return searchHits.stream().map(SearchHit::getContent).collect(Collectors.toList());
 	}
-
-
 }
--- src/main/java/team/rescue/search/service/RecipeSearchService.java
+++ src/main/java/team/rescue/search/service/RecipeSearchService.java
@@ -1,8 +1,9 @@
 package team.rescue.search.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.SearchPage;
 import org.springframework.stereotype.Service;
 import team.rescue.fridge.repository.FridgeRepository;
@@ -18,18 +19,17 @@ public class RecipeSearchService {
 	private final RecipeSearchRepository recipeSearchRepository;
 	private final FridgeRepository fridgeRepository;
 
-	public SearchPage<RecipeInfoDto> searchRecipeByKeyword(
-			String keyword, PageRequest pageRequest
+	public List<RecipeDoc> searchRecipeByKeyword(
+			String keyword, Pageable pageable
 	) {
 
-		SearchPage<RecipeDoc> searchHits =
-				recipeSearchRepository.searchByKeyword(keyword, pageRequest);
+		log.info("키워드 검색 서비스");
+		return recipeSearchRepository.searchByKeyword(keyword, pageable);
 
-		return null;
 	}
 
 	public SearchPage<RecipeInfoDto> searchRecipeByFridge(
-			Long memberId, PageRequest pageRequest
+			Long memberId, Pageable pageable
 	) {
 
 		// 멤버 아이디로 해당 멤버의 냉장고 조회
@@ -37,8 +37,8 @@ public class RecipeSearchService {
 		// 재료 목록 문자열로 변경
 		String ingredients = "";
 		// 문자열 검색
-		SearchPage<RecipeDoc> searchHits =
-				recipeSearchRepository.searchByIngredients(ingredients, pageRequest);
+		List<RecipeDoc> searchHits =
+				recipeSearchRepository.searchByIngredients(ingredients, pageable);
 
 		return null;
 	}
